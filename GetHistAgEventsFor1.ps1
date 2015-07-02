6bc6ef070990b252df9f1fd818359d733f02f8fe
skaboy71 <skaboy71@gmail.com>
2015-07-02 01:15:31 -0700

    adding get 1 user history example script

---
+++ GetHistAgEventsFor1.ps1
@@ -0,0 +1,17 @@
+﻿# include resource file
+$ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path
+. (Join-Path $ScriptDirectory Resource.ps1)
+# Kills $StartStopArray and runs getStartStopArray
+#Remove-Variable StartStopArray -Force
+
+
+
+$startHt = [datetime]"02-06-2014"
+
+$email1 = "yourUser@yourdomain.com"
+
+$userId1 = "This users id from the user table after running the GetUsers script"
+            
+getHistoryFor1User $startHt $email1 $userId1
+
+   
