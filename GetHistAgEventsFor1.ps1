519586b5d9d944274df24441c9f6f4480da8ddba
skaboy71 <skaboy71@gmail.com>
2015-07-02 01:33:19 -0700

    changed

--- GetHistAgEventsFor1.ps1
+++ GetHistAgEventsFor1.ps1
@@ -5,13 +5,15 @@ $ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path
 #Remove-Variable StartStopArray -Force
 
 
-
+# Set Start time(how far back you need to gather all info for)
 $startHt = [datetime]"02-06-2014"
+# create query to get user info from users table
+$userInfoQ = "Select * from users where email = 'yourUser@yourDomain.com';"
+# run query to get info into object
+$info = runquery $userInfoQ
 
-$email1 = "yourUser@yourdomain.com"
-
-$userId1 = "This users id from the user table after running the GetUsers script"
-            
-getHistoryFor1User $startHt $email1 $userId1
+# run function to gather all agreements for this user from the start date by
+# using the "event search" REST for one week at a time.
+getHistoryFor1User $startHt $info.email $info.userId
 
    
